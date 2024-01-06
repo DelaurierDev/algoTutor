3a21aa33638b3d82db4cfe39dda912135553a16c
DelaurierDev <delaurier4@gmail.com>
2024-01-05 19:04:32 -0700

    working on landing page for front end

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Landing = () => {
   return (
-    <div>
-    <div className=''></div>      
+    <div className=' bg-black p-20 h-[100%] flex items-center justify-center'>
+      <div className=' h-[600px] sm:w-[80%] w-[60%] bg-[#abd1b5]'>
+        <p>hello</p>
+        </div>      
     </div>
 
   )
