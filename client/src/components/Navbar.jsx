23525a3897ec9b7cd18115b6602fa9fa83e6a5e3
DelaurierDev <delaurier4@gmail.com>
2024-01-12 09:01:28 -0700

    some nav bar styling and home page styling

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div className='flex justify-between items-center h-[100px] border-b-2 border-[#51291e] max-w-[1150px] mx-auto bg-[#abd1b5] p-5'>
         <div>
-            <h1 className='text-3xl font-bold text-[#310104] border-b-2'>AlgoTutor</h1>
+            <Link to="/home"><h1 className='text-3xl font-bold text-[#310104] border-b-2'>AlgoTutor</h1></Link>
         </div>
         <div>
             <ul className='flex w-[200px] justify-between'>
-                <li className='text-xl text-[#51291e] cursor-pointer'>Searching</li>
-                <li className='text-xl text-[#51291e] cursor-pointer'>Sorting</li>
+                <Link to="/searching"><li className='text-xl text-[#51291e] cursor-pointer'>Searching</li></Link>
+                <Link to="/sorting"><li className='text-xl text-[#51291e] cursor-pointer'>Sorting</li></Link>
             </ul>
         </div>
     </div>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import {Link} from 'react-router-dom'
 
 const Home = () => {
   return (
     <div>
         <Navbar/>
+
+        <div className='h-[600px] flex flex-col items-center justify-center '>
+        <Link to ="/searching" >
+          <button className='h-[70px] w-[300px] bg-[#310104] font-bold rounded text-[#edf4ed] my-7'>Searching Algos</button>
+        </Link>
+        <Link to ="/sorting" >
+          <button className='h-[70px] w-[300px] bg-[#310104] font-bold rounded text-[#edf4ed] my-7'>Sorting Algos</button>
+        </Link>
+        </div>
     </div>
   )
 }
