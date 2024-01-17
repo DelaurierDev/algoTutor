4d8267dca936d779c35c32ceb55db5050a02ad4a
DelaurierDev <delaurier4@gmail.com>
2024-01-16 18:55:36 -0700

    Styling some of the searching page

--- client/src/pages/Searching.jsx
+++ client/src/pages/Searching.jsx
@@ -6,12 +6,21 @@ const Searching = () => {
   return (
     <div>
       <Navbar/>
+      <div className='p-5 text-xl w-[80%] mx-auto max-w-[1000px]'>
+        <p>Being able to search through a set of data is a very important thing to know how to do.  <strong>Click</strong> between the buttons below to view different searching algorithms.</p>
+      </div>
       <div className='h-[600px] flex flex-col items-center justify-center '>
         <Link to ="/searching" >
-          <button className='h-[70px] w-[300px] bg-[#310104] font-bold rounded text-[#edf4ed] my-7'>Searching Algos</button>
+          <button className='h-[70px] w-[300px] bg-[#310104] font-bold rounded text-[#edf4ed] my-7'>Linear Search</button>
         </Link>
         <Link to ="/sorting" >
-          <button className='h-[70px] w-[300px] bg-[#310104] font-bold rounded text-[#edf4ed] my-7'>Sorting Algos</button>
+          <button className='h-[70px] w-[300px] bg-[#310104] font-bold rounded text-[#edf4ed] my-7'>Binary Search</button>
+        </Link>
+        <Link to ="/sorting" >
+          <button className='h-[70px] w-[300px] bg-[#310104] font-bold rounded text-[#edf4ed] my-7'>Interpolation Search</button>
+        </Link>
+        <Link to ="/sorting" >
+          <button className='h-[70px] w-[300px] bg-[#310104] font-bold rounded text-[#edf4ed] my-7'>Jump Search</button>
         </Link>
         </div>
        
