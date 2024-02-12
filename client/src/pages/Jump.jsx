8aba06bbefeaa7f6f7179ee6daaac2057f64a4f7
DelaurierDev <delaurier4@gmail.com>
2024-02-12 10:13:49 -0700

    adding jump search overview

--- client/src/pages/Jump.jsx
+++ client/src/pages/Jump.jsx
@@ -7,7 +7,9 @@ const Jump = () => {
       <Navbar/>
       <h1>Jump Search</h1>
                 <h2 className='text-2xl font-bold'>Overview:</h2>
-                <p>
+                <p> Jump search is a searching algorithm meant to be used on sorted arrays.  You jump in intervals throughout the array.  If the value
+                  at the index you are on is greater than the target.  If it is you preform a linear search from the previous
+                  index.  This method is better than linear search but worse than binary search.
 
                 </p>
                 <h2 className='text-2xl font-bold'>Time Complexity:</h2>
