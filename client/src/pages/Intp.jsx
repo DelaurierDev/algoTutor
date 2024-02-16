c9cbb5911bc487d3816953dd3a8b1960e5cd6575
DelaurierDev <delaurier4@gmail.com>
2024-02-15 20:06:28 -0700

    adding some info the intp page

--- client/src/pages/Intp.jsx
+++ client/src/pages/Intp.jsx
@@ -10,7 +10,9 @@ const Intp = () => {
                 <h1>Interpolation Search</h1>
                 <h2 className='text-2xl font-bold'>Overview:</h2>
                 <p>
-
+                  Interpolation  search is an impovement over Binary Search for instances, where the values in asorted array are uniformly distributed . 
+                  Interpoaltion search constructs new data points within the range of a discrete set of known data points.  Binary search always goes to the middle element to checvk.
+                  On the other hand, interpolation search may go to different locations according to the value of the key being searched.
                 </p>
                 <h2 className='text-2xl font-bold'>Time Complexity:</h2>
                 <p>O(log n) or O(1) if the target is found on the first iteration.</p>
