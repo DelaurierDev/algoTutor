83d89402f9d09d4a038aacecfab71493daead951
DelaurierDev <delaurier4@gmail.com>
2024-02-20 09:34:37 -0700

    setting up the sorting page

---
+++ client/src/pages/Sorting.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const Sorting = () => {
+  return (
+    <div>
+        <Navbar/>
+    </div>
+  )
+}
+
+export default Sorting
